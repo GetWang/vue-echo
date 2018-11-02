431f3d6c717e4e9f758de63dd88ac94fe092058a
GetWang <wang_dresimap@qq.com>
2018-11-02 20:27:31 +0800

    完成今日推荐组件的列表部分样式

--- src/components/today-recommend/today-recommend.vue
+++ src/components/today-recommend/today-recommend.vue
@@ -8,6 +8,34 @@
         <span class="text">一键播放</span>
       </div>
     </div>
+    <ul class="recom-list">
+      <li class="recom-item">
+        <a href="" class="sound-link">
+          <div class="top">
+            <img src="" alt="" class="cover">
+            <div class="duration">03:34</div>
+          </div>
+          <div class="sound-info">
+            <h3 class="sound-name">听妈妈的话 晚点再恋爱吧 「Boys Like You」 </h3>
+            <h4 class="channel"><a href="" class="link">欧美流行指南</a>频道</h4>
+            <ul class="sound-status">
+              <li class="status status-share">
+                <i class="icon"></i>
+                <span class="count">88</span>
+              </li>
+              <li class="status status-like">
+                <i class="icon"></i>
+                <span class="count">88</span>
+              </li>
+              <li class="status status-comment">
+                <i class="icon"></i>
+                <span class="count">88</span>
+              </li>
+            </ul>
+          </div>
+        </a>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -59,6 +87,108 @@
         .text {
           font-size: @font-size-small;
           color: #fff;
+        }
+      }
+    }
+    .recom-list {
+      overflow: hidden;
+      .recom-item {
+        float: left;
+        width: 160px;
+        margin: 0 9px 25px;
+        background-color: @color-background-l;
+        .sound-link {
+          display: block;
+          text-decoration: none;
+          .top {
+            position: relative;
+            .cover {
+              display: block;
+              width: 100%;
+              height: auto;
+              height: 160px;
+              background: red;
+            }
+            .duration {
+              position: absolute;
+              left: 8px;
+              bottom: 5px;
+              height: 14px;
+              padding-left: 19px;
+              font-size: @font-size-small-s;
+              line-height: 14px;
+              color: #fff;
+              background: url("./play-mini.png") no-repeat 0 0;
+              background-size: auto 14px;
+            }
+          }
+          .sound-info {
+            padding: 12px 0 5px;
+            .sound-name {
+              height: 36px;
+              overflow: hidden;
+              margin: 0 15px 15px;
+              font-size: @font-size-small;
+              font-weight: normal;
+              line-height: 18px;
+              color: @color-text-d;
+            }
+            .channel {
+              margin: 0 15px 8px;
+              overflow: hidden;
+              font-size: @font-size-small;
+              line-height: 16px;
+              text-overflow: ellipsis;
+              white-space: nowrap;
+              color: #9a9a9a;
+              .link {
+                margin-right: 5px;
+                text-decoration: none;
+                color: @color-theme;
+              }
+            }
+            .sound-status {
+              font-size: 0;
+              .status {
+                display: inline-block;
+                width: 33.33%;
+                text-align: center;
+                .icon {
+                  display: inline-block;
+                  width: 14px;
+                  height: 14px;
+                  background-image: url("./sound-status-sprite.png");
+                  background-repeat: no-repeat;
+                }
+                .count {
+                  display: block;
+                  margin-top: 2px;
+                  overflow: hidden;
+                  font-size: @font-size-small-s;
+                  line-height: 16px;
+                  text-align: center;
+                  text-overflow: ellipsis;
+                  white-space: nowrap;
+                  color: @color-text-l;
+                }
+                &-share {
+                  .icon {
+                    background-position: 0 -22px;
+                  }
+                }
+                &-like {
+                  .icon {
+                    background-position: -16px -22px;
+                  }
+                }
+                &-comment {
+                  .icon {
+                    background-position: -33px -22px;
+                  }
+                }
+              }
+            }
+          }
         }
       }
     }
