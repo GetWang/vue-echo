8feca03a10afca4a0eca0daff99cccd3fc1504c1
GetWang <wang_dresimap@qq.com>
2018-11-09 22:09:06 +0800

    开发名人类型切换的逻辑

--- src/components/famous-kinds/famous-kinds.vue
+++ src/components/famous-kinds/famous-kinds.vue
@@ -4,12 +4,16 @@
     <div class="kinds-header">
       <h2 class="title">名人分类</h2>
       <ul class="kinds-list">
-        <li class="kinds-item">歌手</li>
+        <li class="kinds-item"
+            :class="{active: currTypeId === kind.typeId}"
+            v-for="kind in famousTypeList"
+            :key="'type-' + kind.typeId"
+            @click="changeType(kind.typeId)">{{kind.typeTitle}}</li>
       </ul>
     </div>
     <ul class="kinds-user-list">
-      <li class="kinds-user-item">
-        <a href="" class="famous-link">JC陈泳彤</a>
+      <li class="kinds-user-item" v-for="user in famousListByType" :key="user.id">
+        <a :href="'#/user/' + user.id" class="famous-link">{{user.name}}</a>
       </li>
     </ul>
   </div>
@@ -17,22 +21,37 @@
 
 <script type="text/ecmascript-6">
   import {getFamousTypeList, getFamousListByType} from 'api/user'
-  // import {STATUS_OK} from 'api/config'
+  import {STATUS_OK} from 'api/config'
+  import User from 'common/js/user'
 
   // 根据名人类别获取名人的个数限制
   const limit = 44
+  // 初始名人类别 Id
+  const initTypeId = 1
 
   export default {
     name: 'FamousKinds',
+    data () {
+      return {
+        // 名人类别列表
+        famousTypeList: [],
+        // 当前的名人类别 Id
+        currTypeId: initTypeId,
+        // 某个分类的名人列表
+        famousListByType: []
+      }
+    },
     created () {
       this._getFamousTypeList()
-      this._getFamousListByType(1, limit)
+      this._getFamousListByType(initTypeId, limit)
     },
     methods: {
       /* 获取名人页面名人分类的 type 列表 */
       _getFamousTypeList () {
         getFamousTypeList().then(res => {
-          console.log('res-type', res)
+          if (res.status === STATUS_OK) {
+            this.handleFamousTypeList(res)
+          }
         }).catch(err => {
           console.log('api/getFamousTypeList error', err)
         })
@@ -40,10 +59,37 @@
       /* 根据名人类别获取名人页面的名人列表 */
       _getFamousListByType (famous_type, limit) {
         getFamousListByType(famous_type, limit).then(res => {
-          console.log('res-famous-list', res)
+          if (res.status === STATUS_OK) {
+            this.handleFamousListByType(res)
+          }
         }).catch(err => {
           console.log('api/getFamousListByType error', err)
         })
+      },
+      /* 处理名人分类的 type 列表 */
+      handleFamousTypeList (data) {
+        data.lists.forEach((item) => {
+          this.famousTypeList.push({
+            typeId: item.famous_type,
+            typeTitle: item.famous_type_title
+          })
+        })
+      },
+      /* 处理某个类别的名人数据 */
+      handleFamousListByType (data) {
+        // 清空某个分类的名人列表
+        this.famousListByType.splice(0, this.famousListByType.length)
+        data.famous_users[0].users.forEach((item) => {
+          this.famousListByType.push(new User(item))
+        })
+      },
+      // 切换名人类型
+      changeType (typeId) {
+        if (typeId === this.currTypeId) {
+          return
+        }
+        this.currTypeId = typeId
+        this._getFamousListByType(typeId, limit)
       }
     }
   }
