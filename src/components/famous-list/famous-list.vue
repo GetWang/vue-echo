a058efd6d0f8a73cdf0eaff30ae1db0ed94ac730
GetWang <wang_dresimap@qq.com>
2018-11-13 16:28:59 +0800

    获取名人列表数据并做处理

--- src/components/famous-list/famous-list.vue
+++ src/components/famous-list/famous-list.vue
@@ -1,16 +1,16 @@
 <template>
   <main class="famous-list">
     <div class="content">
-      <h2 class="list-name">echo 群星</h2>
+      <h2 class="list-name">{{currListTitle}}</h2>
       <ul class="user-list">
-        <li class="user-item">
-          <a href="" class="user-link">
-            <img src="" alt="" class="avatar">
+        <li class="user-item" v-for="user in userList" :key="user.id">
+          <a :href="'#/user/' + user.id" class="user-link">
+            <img :src="user.avatar_150" :alt="user.name" class="avatar">
           </a>
           <p class="user-name">
-            <a href="" class="link">GEM鄧紫棋</a>
+            <a :href="'#/user/' + user.id" class="link">{{user.name}}</a>
           </p>
-          <p class="user-desc">香港知名歌手香港知名歌手香港知名歌手</p>
+          <p class="user-desc">{{user.desc}}</p>
         </li>
       </ul>
     </div>
@@ -18,8 +18,81 @@
 </template>
 
 <script type="text/ecmascript-6">
+  import {getTopUser, getLatestUser, getRecomUser, getDailyHotUser} from 'api/user'
+  import {STATUS_OK} from 'api/config'
+  import User from 'common/js/user'
+
+  // 名人列表页面的类型名字
+  const famousListNames = ['top-user', 'latest-user', 'recommend-user', 'daily-hot-user']
+  // 名人列表页面的标题
+  const famousListTitles = ['echo 群星', '新入驻', 'echo 推荐', '24小时热门']
+
   export default {
-    name: 'FamousList'
+    name: 'FamousList',
+    data () {
+      return {
+        // 当前名人列表页面的类型名字
+        currListName: '',
+        // 当前名人列表页面的标题
+        currListTitle: '',
+        // 当前列表页数
+        currPage: 1,
+        // 名人列表
+        userList: []
+      }
+    },
+    created () {
+      this.checkCurrListName()
+      this._getUserList(1, 16)
+    },
+    methods: {
+      /* 检查名人列表页面类型的名字是否存在 */
+      checkCurrListName () {
+        this.currListName = this.$route.params.listName
+        let len = famousListNames.length
+        for (let i = 0; i < len; i++) {
+          if (this.currListName === famousListNames[i]) {
+            this.currListTitle = famousListTitles[i]
+            return
+          }
+        }
+        this.$router.replace('/famous')
+      },
+      /* 获取当前名人列表页面数据 */
+      _getUserList (page, limit) {
+        if (this.currListName === famousListNames[0]) {
+          getTopUser(page, limit).then(res => {
+            if (res.status === STATUS_OK) {
+              this.handleUserList(res)
+            }
+          })
+        } else if (this.currListName === famousListNames[1]) {
+          getLatestUser(page, limit).then(res => {
+            if (res.status === STATUS_OK) {
+              this.handleUserList(res)
+            }
+          })
+        } else if (this.currListName === famousListNames[2]) {
+          getRecomUser(page, limit).then(res => {
+            if (res.status === STATUS_OK) {
+              this.handleUserList(res)
+            }
+          })
+        } else if (this.currListName === famousListNames[3]) {
+          getDailyHotUser(page, limit).then(res => {
+            if (res.status === STATUS_OK) {
+              this.handleUserList(res)
+            }
+          })
+        }
+      },
+      /* 处理当前名人列表数据 */
+      handleUserList (data) {
+        data.lists.forEach((item) => {
+          this.userList.push(new User(item))
+        })
+      }
+    }
   }
 </script>
 
@@ -41,7 +114,7 @@
         font-size: @font-size-large-x;
         font-weight: normal;
         line-height: 36px;
-        color: @color-text;        
+        color: @color-text;
       }
       .user-list {
         width: 693px;
@@ -64,7 +137,6 @@
               display: block;
               width: 100%;
               height: 100%;
-              background: red;
             }
           }
           .user-name {
